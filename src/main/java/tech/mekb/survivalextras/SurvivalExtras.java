bcdfbb088959f3ce4f1c064a0d390c1aac19d31c
mekb the turtle <mekbgithub@icloud.com>
2022-09-12 19:51:39 +1000

    initial commit

---
+++ src/main/java/tech/mekb/survivalextras/SurvivalExtras.java
@@ -0,0 +1,9 @@
+package tech.mekb.survivalextras;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalExtras extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
